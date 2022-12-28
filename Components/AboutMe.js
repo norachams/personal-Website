ce729775e67e3e806a7a936d6b4def8410d19dec
nora chams <nchamseddin@gmail.com>
2022-12-28 13:47:53 -0500

    updated about me section

--- Components/AboutMe.js
+++ Components/AboutMe.js
@@ -8,17 +8,16 @@ export default function Home() {
     <h2 className="about-me-title ">About</h2>
     <div className="about-me-text-and-image flex bg-transparent">
       <div className="about-me-text">
-      I am a second-year dual degree university student studying computer science and business adminstration.
-          I enjoy problem-solving and challenging myself by taking on diffuclt projects that I am able to learn from. 
+          Hello! My name is Nora a second-year dual degree university student studying computer science and business administration. 
+          I have a strong passion for problem-solving and enjoy challenging myself by taking on difficult projects that I am able to learn from.
           <br></br>
           <br></br>
-          This past summer I had the privilege to intern at an agency called Klick Health as a web developer and in the fall I joined MLH as a fellow. 
-          Throughout the years I participated as a software developer in many hackathons and school clubs and built web apps that solve real problems. 
-          Recently, I have been very interested in the world of machine learning and AI. 
-          I plan on focusing and exploring this interest by building projects and doing research. 
+          This past summer, I had the privilege to intern at an agency called Klick Health as a web developer, and in the fall I joined MLH as a fellow. 
+          Throughout the years, I have participated in numerous hackathons and school clubs as a software developer, building web apps that solve real problems.
+          Recently, I have become very interested in the world of machine learning and AI, and I plan on focusing on and exploring this interest by building projects and doing research.
           <br></br>
           <br></br>
-          Outside of school and programming I really enjoy rock climbing. I am also a big a cat person and have two cats.
+          In addition to my studies and professional experience, I am also an avid rock climber and a cat person - I have two cats, Caramella and Woody.
       </div>
       <div className="image-background">
         <img className="about-me-image" src="/me.png" alt="About Me Image" />
